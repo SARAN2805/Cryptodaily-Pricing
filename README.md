ef97a07c9a6697eea6a777e4378c84310b9936bc
SARAN2805 <saranemail2805@gmail.com>
2023-02-17 20:47:32 +0530

    first commit

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 
-# Fuel Price In India
+# CRYPTO Price In India
 
 
 
@@ -12,7 +12,7 @@
 
 ## Intro To The Project
 
-This is an site where API is not fetched on any Site ,this site is web scrapped and we created an api on it,this is site supervised under our Mobile App Lab And it is developed by Nifaz and Samuel
+This is an site where API is not fetched on any Site ,this site is web scrapped and we created an api on it,this is site supervised under our Mobile App Lab
 
 
 ## Reference
@@ -20,7 +20,7 @@ This is an site where API is not fetched on any Site ,this site is web scrapped 
   we web scrapped this using python module named beautiful soup,
   and using this we created our own api and used it in our site
   ,we fetched the price details from 
-  =>https://economictimes.indiatimes.com/wealth/fuel-price/petrol
+  =>https://coinmarketcap.com/
   Before Compiling This Project you Should Install Some Py-Libraries=>
     
     -beautifulsoup4 
@@ -28,8 +28,3 @@ This is an site where API is not fetched on any Site ,this site is web scrapped 
     -requests
 To run the project you have to run => app.py,
 Run on this server =>http://127.0.0.1:5001/
-## Authors
-
-- [@Samuel](https://www.github.com/renjithsamuel)
-- [@Nifaz](https://www.github.com/nifazzz10)
-
--- app.py
+++ app.py
@@ -3,15 +3,16 @@ from bs4 import BeautifulSoup
 from nbformat import write
 import requests
 import json
+import pandas as pd 
+#import aspose
+#from aspose import jpype
 
 
 app = Flask(__name__)
-
 @app.route("/")
-# @app.route("/home")
 
-# def home():
-#     return render_template('index.html')
+def index():
+    return render_template('index.html')
 
 @app.route("/get_content")
 def get_content():
@@ -19,16 +20,32 @@ def get_content():
     response = requests.get(url).text
     soup = BeautifulSoup(response, "html.parser")
     table = soup.find_all("tr")[1:]
-    states = []
+    Crypto = []
 
     for row in table:
         row = list(filter(lambda x: x != "\n", row))
-        state_name = row[2].text
-        state_price = row[3].text
-        states.append({"State":state_name,"Price":state_price})
-
+        crypto_name = row[2].text
+        crypto_price = row[3].text
+        Crypto.append({"Name":crypto_name,"Price":crypto_price})
     # return states
-    return render_template("prices.html", states = states)
+    
+    with open('data.json', 'w', encoding='utf-8') as f:
+        json.dump(Crypto, f , ensure_ascii=False, indent=4)
+        
+    return render_template("prices.html", Crypto = Crypto)
 
+@app.route("/excel")
+def excel():
+    url = "http://127.0.0.1:5001/get_content"
+    response = requests.get(url).text
+    soup = BeautifulSoup(response, "html.parser")
+    table1 = soup.find_all("table")[1:]
+    Data_Crypto= soup.findAll('div', {'class':'main'})
+    result1 = [(para.text).strip().split(',') for para in table1] 
+    df = pd.DataFrame(result1)
+    print(df)
+    df.to_excel('ouput.xlsx', index=False)
+#     return render_template("prices.html")
+    
 if __name__ == '__main__':
     app.run(debug=True, port=5001)
